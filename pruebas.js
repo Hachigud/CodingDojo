64f4d5217f8b5c94e4d584c2aa201bb84e8c22ae
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-09-26 11:03:11 -0300

    agrego ejercicio calculadora

--- pruebas.js
+++ pruebas.js
@@ -11,3 +11,27 @@ for(var i=1; i<5; i++) {
  console.log(x);
  */
 
+
+
+ /* 
+ Dado el arreglo ["Perro","Gato","Vaca","Alpaca","Chinchilla"]
+convertirlo en éste: ["Vaca","Alpaca","Chinchilla","Perro","Gato"] 
+*/
+
+/* 
+let array = ["Perro","Gato","Vaca","Alpaca","Chinchilla"];
+let arrayAux = [];
+let arrayAux2 = [];
+let arrayFinal = [];
+for (let i = 2; i< array.length; i++){
+    arrayAux[i-2] = array[i];
+}
+
+for (i = 0; i< 2; i++){
+    arrayAux2[i] = array[i];
+}
+
+arrayFinal = arrayAux.concat(arrayAux2);
+
+console.log(arrayFinal);
+*/
