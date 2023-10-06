429fa720566d3192e7bfdf9f027ad7ac1e646ebb
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-10-06 10:33:34 -0300

    agrego ejercicios dia 5

---
+++ semana 5/dia 5/Fundamentos/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// TIPOS DE DATOS
+Console.WriteLine("Hello, World!");
+short edad = 15;
+int habitantes = 123456;
+long kilometros = 10000000000;
+
+float nota = 6.5f;
+double pi = 3.1416;
+
+string nombre = "rafael";
+char letra = 'a'; // estrictamente comillas simples
+
+bool aprobado = true;
+
+
+const float iva = 0.14f;
+
+
+
+
+
+if(edad >=18){
+    Console.WriteLine(nombre + " Si puedes manejar");
+}else{
+    int años = 18 - edad;
+    Console.WriteLine(nombre + " No puedes manejar te faltan " + años + " años para manejar");
+    Console.WriteLine($"{nombre} no tienes edad para manejar, te faltan {años} para poder hacerlo");  // string con formato ( permite agregar variables dentro mas facil)
+}
+
+Console.WriteLine("Me llamo {0}, tengo " + 28 + " años", "Tim");
+Console.WriteLine($"Me llamo {0}, tengo " + 28 + " años", "Tim");
+
+Random rand = new Random();
+for(int val = 0; val < 10; val++)
+{
+    //Prints the next random value between 2 and 8
+    Console.WriteLine(rand.Next(2,8));
+}
---
+++ semana 5/dia 5/fizzbuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿//1.-
+
+Console.WriteLine("1.- Create a Loop that prints all values from 1-255");
+
+for (int i = 1 ; i<= 255;i++){
+    Console.WriteLine(i);
+}
+
+
+Console.WriteLine("2.- Create a new loop that prints all values from 1-100 that are divisible by 3 or 5, but not both");
+
+
+for (int i = 1 ; i<= 100;i++){
+    
+    if(i%3 == 0 || i%5 ==0){
+        Console.WriteLine(i);
+        
+    }
+}
+
+Console.WriteLine("3.-Modify the previous loop to print 'Fizz' for multiples of 3, 'Buzz' for multiples of 5, and 'FizzBuzz' for numbers that are multiples of both 3 and 5");
+for (int i = 1 ; i<= 100;i++){
+    if(i%3 == 0 && i%5 ==0){
+        Console.WriteLine("FizzBuzz");       
+    }else if(i%3 == 0){
+         Console.WriteLine("Fizz");      
+    }else if(i%5 ==0){
+        Console.WriteLine("Buzz");
+      
+    }
+}
+
+Console.WriteLine("4.- If you used 'for' loops for your solution, try doing the same with 'while' loops. Vice-versa if you used 'while' loops!");
+int a = 1 ;
+while ( a<= 100){
+    if(a%3 == 0 && a%5 ==0){
+        Console.WriteLine("FizzBuzz");       
+    }else if(a%3 == 0){
+         Console.WriteLine("Fizz");      
+    }else if(a%5 ==0){
+        Console.WriteLine("Buzz");
+      
+    }
+    a++;
+}
