98ccaf7f8173ef06262a66cc73634dd23f0b101f
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-10-10 12:56:44 -0300

    agrego ejercicio boxin semana 6 dia 1

---
+++ semana 6/boxin/Program.cs
@@ -0,0 +1,16 @@
+﻿List<object> objeto = new List<object>();
+objeto.Add(7);
+objeto.Add(28);
+objeto.Add(-1);
+objeto.Add(true);
+objeto.Add("chair");
+
+int suma = 0;
+foreach(object iterador in objeto){
+    Console.Write(iterador + "\n");
+    if(iterador is int){
+        suma += (int)iterador;
+    }
+}
+
+Console.Write(suma);
