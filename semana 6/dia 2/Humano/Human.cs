0ec94118b2c8d56dc3d3eaf3ecb68362ad150205
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-10-11 12:23:57 -0300

    agrego ejercicio humans semana 6 dia 2

---
+++ semana 6/dia 2/Humano/Human.cs
@@ -0,0 +1,70 @@
+class Human
+{
+    // Properties for Human
+    public string Name;
+    public string _Name
+     {
+         get { return Name; }
+         set { Name = value; }
+     }
+    public int Strength;
+
+    public int _Strength
+     {
+         get { return Strength; }
+         set { Strength = value; }
+     }
+    public int Intelligence;
+
+    public int _Intelligence
+     {
+         get { return Intelligence; }
+         set { Intelligence = value; }
+     }
+    public int Dexterity;
+
+     public int _Dexterity
+     {
+         get { return Dexterity; }
+         set { Dexterity = value; }
+     }
+    public int Health;
+
+      public int _Health
+     {
+         get { return Health; }
+         set { Health = value; }
+     }
+    
+    public Human(string Name){
+        this.Name = Name;
+        Strength = 3;    
+        Intelligence = 3;
+        Dexterity = 3;
+        Health = 100;
+    }
+
+    public Human(string Name,int Strength, int Intelligence, int Dexterity, int Health){
+        this.Name = Name;
+        this.Strength = Strength;    
+        this.Intelligence = Intelligence;
+        this.Dexterity = Dexterity;
+        this.Health = Health;
+    }
+     
+    
+    public int Attack(Human target,Human atacante)
+    {
+        target._Health -= (atacante._Strength * 3);
+        Console.Write($"El jugador {atacante._Name} ataco a {target._Name} haciendo {atacante._Strength * 3} de daño!");
+        return(target._Health);
+    }
+
+      public void ImprimeInformacion(){
+        Console.WriteLine($"Nombre: {Name}");
+        Console.WriteLine($"Strength: {Strength}");
+        Console.WriteLine($"Intelligence: {Intelligence}");
+        Console.WriteLine($"Dexterity: {Dexterity}");
+        Console.WriteLine($"Health: {Health}");
+    }
+}
---
+++ semana 6/dia 2/Humano/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string nombre = "juan";
+string nombre2 = "Pedro";
+int Strength = 10;
+int Intelligence =10;
+int Dexterity = 10;
+int Health = 200;
+Human jugador  = new Human(nombre);
+//jugador.ImprimeInformacion();
+Human jugador2  = new Human(nombre2,Strength,Intelligence,Dexterity,Health);
+//jugador2.ImprimeInformacion();
+
+int vidaJugador = jugador.Attack(jugador2,jugador);
+Console.Write("\n");
+Console.Write($"Vida restante: {vidaJugador}");
+Console.Write("\n");
+int vidaJugador2 = jugador2.Attack(jugador,jugador2);
+Console.Write("\n");
+Console.Write($"Vida restante: {vidaJugador2}");
+
+
