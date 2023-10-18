ba2043d4ed4cb07d84affceb56a13080c51d65ad
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-10-18 12:06:23 -0300

    agrego ejercicio semana 7 dia 3 formulario mvc

---
+++ semana 7/formulariomvc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using formulariomvc.Models;
+
+namespace formulariomvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet] 
+    [Route("/")] 
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpGet] 
+    [Route("privacy")] 
+    public IActionResult Privacy()
+    {
+        return View("Privacy");
+    }
+
+
+
+
+    [HttpGet] 
+    [Route("datos")]
+    public IActionResult Datos(Formulario formulario){
+        return View("Datos",formulario);
+    }
+
+    [HttpPost("formulario")]
+    public RedirectToActionResult formulario (Formulario formulario){
+
+        return RedirectToAction("Datos",formulario);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ semana 7/formulariomvc/Models/FormularioModel.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+namespace formulariomvc.Models;
+
+public class Formulario
+{
+    public string Nombre {get;set;}
+    public string Dojo {get;set;}
+    public string Lenguaje {get;set;}
+    public string? Comentario {get;set;}
+}
