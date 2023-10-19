8333b33be53b6ec5c572735d653620e1632efd6a
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-10-19 17:08:49 -0300

    agrego ejercicio randomcode semana 7 dia 4

---
+++ semana 7/randomcode/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function generar(){
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let cantidad = document.getElementById("cantidadCodes");
+    let cantidadTotal = cantidad.innerText;
+    cantidadTotal ++;
+    cantidad.innerText = cantidadTotal;
+    let codigo = document.getElementById("codigo");
+    let newCode = "";
+    for ( let i = 0; i < 14; i++ ) {
+        newCode += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    codigo.value = newCode;
+}
