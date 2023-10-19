c9a7f37333ac2c11a665b430754e87bb95661333
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-10-19 11:22:41 -0300

    agrego ejercicio semana 7 dia 4 formulario con validaciones

---
+++ semana 7/formulariomvcvalidator/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using formulariomvcvalidator.Models;
+
+namespace formulariomvcvalidator.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet] 
+    [Route("/")] 
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpGet] 
+    [Route("privacy")] 
+    public IActionResult Privacy()
+    {
+        return View("Privacy");
+    }
+
+
+
+
+    [HttpGet] 
+    [Route("datos")]
+    public IActionResult Datos(Formulario formulario){
+        return View("Datos",formulario);
+    }
+
+    [HttpPost("formulario")]
+    public IActionResult formulario (Formulario formulario){
+        if(ModelState.IsValid)    
+        {  
+             return RedirectToAction("Datos",formulario);
+        }else
+        {
+            return  View("Index");
+        }
+       
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ semana 7/formulariomvcvalidator/Models/FormularioModel.cs
@@ -0,0 +1,18 @@
+#pragma warning disable CS8618
+namespace formulariomvcvalidator.Models;
+using System.ComponentModel.DataAnnotations;
+public class Formulario
+{
+    [Required(ErrorMessage="El campo Nombre es requerido")]
+    [MinLength(3, ErrorMessage="El nombre debe contener 3 o mas caracteres")]
+    public string Nombre {get;set;}
+
+    [Required(ErrorMessage="El campo Dojo es requerido")]
+    public string Dojo {get;set;}
+
+    [Required(ErrorMessage="El campo Lenguaje favorito es requerido")]
+    public string Lenguaje {get;set;}
+
+    [MinLength(20, ErrorMessage="El comentario debe contener 20 o mas caracteres")]
+    public string? Comentario {get;set;}
+}
